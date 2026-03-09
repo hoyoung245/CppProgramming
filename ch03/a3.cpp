d850e6cd586f7e11f8dafe39366ab708daa3b9d5
hoyoung245 <245hoyoung@gmail.com>
2026-03-09 21:32:38 +0900

    Update a3.cpp

--- ch03/a3.cpp
+++ ch03/a3.cpp
@@ -1,1 +1,15 @@
 
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Name : hong gil dong" << endl;
+    cout << "Address : gunsan daehakro 558" << endl;
+    cout << "ID : 201012" << endl;
+    cout << "Car : Benz" << endl;
+
+    return 0;
+}
