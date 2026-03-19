bd5750eea5eb7c8751a51ad904b8146bd4469dd8
hoyoung245 <245hoyoung@gmail.com>
2026-03-19 19:44:33 +0900

    Update a6.cpp

--- ch06/a6.cpp
+++ ch06/a6.cpp
@@ -28,41 +28,6 @@ int main() {
 
 // 실습과제3 **********************************************
 
-#include <iostream>
-using namespace std;
-class Triangle {
-public:
-	double s;
-	double h;
-	double getArea();
-};
-double Triangle::getArea() {
-	return s * h * 0.5;
-}
-int main() {
-	Triangle t;
-	t.s = 3.0;
-	t.h = 5.0;
-	cout << "삼각형의 면적은 " << t.getArea();
-	return 0;
-}
+
 // 실습과제4 **********************************************
 
-#include <iostream>
-using namespace std;
-class Triangle {
-public:
-	double s;
-	double h;
-	double getArea();
-};
-double Triangle::getArea() {
-	return s * h * 0.5;
-}
-int main() {
-	Triangle t;
-	t.s = 3.0;
-	t.h = 5.0;
-	cout << "삼각형의 면적은 " << t.getArea();
-	return 0;
-}
