0d1e2c05bfe4d80309ecc29ea9f53026dccf3eb3
hoyoung245 <245hoyoung@gmail.com>
2026-04-08 09:31:46 +0900

    Update a12.cpp

--- ch12/a12.cpp
+++ ch12/a12.cpp
@@ -45,3 +45,40 @@ int main() {
 }
 
 // 실습과제5 **********************************************
+
+#include <iostream>
+using namespace std;
+
+class Sphere {
+    int radius;
+public:
+    void setRadius(int r) {
+        radius = r;
+    }
+
+    double getVolume() {
+        return 4.0 / 3 * 3.14 * radius * radius * radius;
+    }
+};
+
+int main() {
+    int n;
+    cout << "생성하고자 하는 구의 개수: ";
+    cin >> n;
+
+    Sphere* arr = new Sphere[n];
+
+    for (int i = 0; i < n; i++) {
+        int r;
+        cout << "구" << i + 1 << "의 반지름: ";
+        cin >> r;
+        arr[i].setRadius(r);
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << "구" << i + 1 << "의 부피 " << arr[i].getVolume() << endl;
+    }
+
+    delete[] arr;
+    return 0;
+}
