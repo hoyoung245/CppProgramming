951f1123097f2e0e43cbf86d1648fdfd85a4066e
hoyoung245 <245hoyoung@gmail.com>
2026-05-10 20:30:27 +0900

    Update a15.cpp

--- ch15/a15.cpp
+++ ch15/a15.cpp
@@ -6,6 +6,29 @@
 
 // 실습과제2 **********************************************
 
+#include<iostream>
+using namespace std;
+
+void add2(int& value);
+
+int main(void)
+{
+    int number;
+
+    cout << "정수를 입력하세요 : ";
+    cin >> number;
+
+    add2(number);
+
+    cout << "2만큼 증가한 값 : " << number << endl;
+
+    return 0;
+}
+
+void add2(int& value)
+{
+    value += 2;
+}
 
 
 // 실습과제3 **********************************************
