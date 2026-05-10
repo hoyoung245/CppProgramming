7a163357ea9e92441bff4efefb4f7616fefe5143
hoyoung245 <245hoyoung@gmail.com>
2026-05-10 20:32:39 +0900

    Update a15.cpp

--- ch15/a15.cpp
+++ ch15/a15.cpp
@@ -32,3 +32,36 @@ void add2(int& value)
 
 
 // 실습과제3 **********************************************
+
+#include<iostream>
+using namespace std;
+
+void swap(int& a, int& b);
+
+int main()
+{
+    int x, y;
+
+    cout << "정수x를 입력 하시오: ";
+    cin >> x;
+
+    cout << "정수y를 입력 하시오: ";
+    cin >> y;
+
+    cout << "swap함수 호출 전 x=" << x << ", y=" << y << endl;
+
+    swap(x, y);
+
+    cout << "swap함수 호출 후 x=" << x << ", y=" << y << endl;
+
+    return 0;
+}
+
+void swap(int& a, int& b)
+{
+    int temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+}
