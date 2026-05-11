6e4b609e16db8c41b7fbce49e472daa1ee6f0bd5
hoyoung245 <245hoyoung@gmail.com>
2026-05-11 10:16:28 +0900

    Update a15.cpp

--- ch15/a15.cpp
+++ ch15/a15.cpp
@@ -31,43 +31,28 @@ void Uppercase(string& s) {
     }
 }
 // 실습과제3 **********************************************
+#include <iostream>
+#include <string>
 
-#include<iostream> // 입출력 사용
-using namespace std; // std 네임스페이스 사용
+using namespace std;
 
-// swap 함수 선언
-void swap(int& a, int& b);
+void SwapString(string& a, string& b);
 
-int main()
-{
-    int x, y; // 두 정수 변수 선언
+int main() {
+    string s1("hello");
+    string s2("world");
 
-    cout << "정수x를 입력 하시오: "; // x 입력 안내
-    cin >> x; // x 입력
+    cout << "교환전 문자열 " << s1 << " " << s2 << endl;
 
-    cout << "정수y를 입력 하시오: "; // y 입력 안내
-    cin >> y; // y 입력
+    SwapString(s1, s2);
 
-    // swap 함수 호출 전 값 출력
-    cout << "swap함수 호출 전 x=" << x << ", y=" << y << endl;
+    cout << "교환후 문자열 " << s1 << " " << s2 << endl;
 
-    swap(x, y); // 두 변수 값 교환
-
-    // swap 함수 호출 후 값 출력
-    cout << "swap함수 호출 후 x=" << x << ", y=" << y << endl;
-
-    return 0; // 프로그램 종료
+    return 0;
 }
 
-// 두 정수 값을 교환하는 함수
-void swap(int& a, int& b)
-{
-    int temp; // 임시 변수 선언
-
-    temp = a; // a 값을 temp에 저장
-    a = b; // b 값을 a에 저장
-    b = temp; // temp 값을 b에 저장
-}
+void SwapString(string& a, string& b) {
+    string temp;
 
     temp = a;
     a = b;
@@ -76,36 +61,3 @@ void swap(int& a, int& b)
 
 // 실습과제4 **********************************************
 
-#include<iostream> // 입출력 사용
-using namespace std; // std 네임스페이스 사용
-
-// 함수 선언
-void get_parts(double number, int& integerPart, double& decimalPart);
-
-int main()
-{
-    double number; // 실수 저장 변수
-    int integerPart; // 정수부 저장 변수
-    double decimalPart; // 소수부 저장 변수
-
-    cout << "실수를 입력하시오 : "; // 입력 안내
-    cin >> number; // 실수 입력
-
-    // 정수부와 소수부 계산 함수 호출
-    get_parts(number, integerPart, decimalPart);
-
-    // 정수부 출력
-    cout << "정수부 : " << integerPart << endl;
-
-    // 소수부 출력
-    cout << "소수부 : " << decimalPart << endl;
-
-    return 0; // 프로그램 종료
-}
-
-// 정수부와 소수부를 구하는 함수
-void get_parts(double number, int& integerPart, double& decimalPart)
-{
-    integerPart = (int)number; // 정수부 저장
-    decimalPart = number - integerPart; // 소수부 계산
-}
