37243f5ef4de5cb5ae7903017aada9738049919a
hoyoung245 <245hoyoung@gmail.com>
2026-05-13 10:48:10 +0900

    Update a16.cpp

--- ch16/a16.cpp
+++ ch16/a16.cpp
@@ -49,7 +49,86 @@ int main() {
 
     return 0;
 }
+
+
 // 실습과제3 **********************************************
 
+#include <iostream>
+using namespace std;
+
+class MyStack {
+    int* p;
+    int size;
+    int tos;
+
+public:
+    MyStack() {
+        size = 10;
+        tos = 0;
+        p = new int[size];
+    }
+
+    MyStack(int size) {
+        this->size = size;
+        tos = 0;
+        p = new int[size];
+    }
+
+    MyStack(const MyStack& src) {
+        this->size = src.size;
+        this->tos = src.tos;
+
+        p = new int[size];
+
+        for (int i = 0; i < tos; i++) {
+            p[i] = src.p[i];
+        }
+    }
+
+    ~MyStack() {
+        delete[] p;
+    }
+
+    bool push(int n) {
+        if (tos == size)
+            return false;
+
+        p[tos] = n;
+        tos++;
+
+        return true;
+    }
+
+    bool pop(int& n) {
+        if (tos == 0)
+            return false;
+
+        tos--;
+        n = p[tos];
+
+        return true;
+    }
+};
+
+int main() {
+    MyStack a(10);
+
+    a.push(10);
+    a.push(20);
+
+    MyStack b = a;
+
+    b.push(30);
+
+    int n;
+
+    a.pop(n);
+    cout << "스택 a에서 팝한 값 " << n << endl;
+
+    b.pop(n);
+    cout << "스택 b에서 팝한 값 " << n << endl;
+
+    return 0;
+}
 
 // 실습과제4 **********************************************
