80344c01d99c35c241e99c6fd370f316f2513672
hoyoung245 <245hoyoung@gmail.com>
2026-05-14 20:37:48 +0900

    Update a16.cpp

--- ch16/a16.cpp
+++ ch16/a16.cpp
@@ -132,3 +132,78 @@ int main() {
 }
 
 // 실습과제4 **********************************************
+
+#include <iostream>
+using namespace std;
+
+class Collector {
+    int* p;
+    int size = 0;
+
+public:
+    Collector(int size, int values[]);
+    Collector(const Collector& src);
+    ~Collector();
+
+    void show();
+    int getSize() { return size; }
+    int get(int index) { return p[index]; }
+};
+
+Collector::Collector(int size, int values[]) {
+    this->size = size;
+
+    p = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        p[i] = values[i];
+    }
+}
+
+Collector::Collector(const Collector& src) {
+    this->size = src.size;
+
+    p = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        p[i] = src.p[i];
+    }
+}
+
+Collector::~Collector() {
+    delete[] p;
+}
+
+void Collector::show() {
+    cout << "데이터 수 " << size << ": ";
+
+    for (int i = 0; i < size; i++) {
+        cout << p[i] << " ";
+    }
+
+    cout << endl;
+}
+
+double calcAvg(Collector c) {
+    int sum = 0;
+
+    for (int i = 0; i < c.getSize(); i++) {
+        sum += c.get(i);
+    }
+
+    return (double)sum / c.getSize();
+}
+
+int main() {
+    int temp[] = { 69, 70, 71, 72, 74 };
+
+    Collector weight(4, temp);
+
+    double avg = calcAvg(weight);
+
+    weight.show();
+
+    cout << "평균은 " << avg << endl;
+
+    return 0;
+}
