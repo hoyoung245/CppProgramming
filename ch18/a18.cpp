12b9c7dd35cd86767c96224f7d06f50031be62cb
hoyoung245 <245hoyoung@gmail.com>
2026-05-18 21:44:47 +0900

    Update a18.cpp

--- ch18/a18.cpp
+++ ch18/a18.cpp
@@ -6,6 +6,36 @@
 
 // 실습과제2 **********************************************
 
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+    int width, height;
+
+public:
+    Rectangle(int w = 1, int h = 1) {
+        width = w;
+        height = h;
+    }
+
+    void show() {
+        cout << "사각형 폭은 " << width
+             << " 높이는 " << height << endl;
+    }
+};
+
+int main() {
+    Rectangle rect0;
+    rect0.show();
+
+    Rectangle rect1(10);
+    rect1.show();
+
+    Rectangle rect2(10, 20);
+    rect2.show();
+
+    return 0;
+}
 // 실습과제3 **********************************************
 
 // 실습과제4 **********************************************
