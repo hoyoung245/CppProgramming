7b07e397d7eb76f786e65e05b6c43c3b1b57276f
hoyoung245 <245hoyoung@gmail.com>
2026-05-18 21:45:14 +0900

    Update a18.cpp

--- ch18/a18.cpp
+++ ch18/a18.cpp
@@ -38,7 +38,41 @@ int main() {
 }
 // 실습과제3 **********************************************
 
-// 실습과제4 **********************************************
+#include <iostream>
+using namespace std;
+
+class Point3D {
+    int x, y, z;
+
+public:
+    Point3D(int a = 0, int b = 0, int c = 0) {
+        x = a;
+        y = b;
+        z = c;
+    }
+
+    void show() {
+        cout << "3차원 점의 좌표는 (" 
+             << x << "," << y << "," << z << ")" << endl;
+    }
+};
+
+int main() {
+    Point3D p0;
+    p0.show();
+
+    Point3D p1(1);
+    p1.show();
+
+    Point3D p2(1, 2);
+    p2.show();
+
+    Point3D p3(1, 2, 3);
+    p3.show();
+
+    return 0;
+}
+
 
 
 
