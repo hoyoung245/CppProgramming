2d61f407d2e2f521185474d1f58d9007710a4813
hoyoung245 <245hoyoung@gmail.com>
2026-05-21 19:44:41 +0900

    Update a19.cpp

--- ch19/a19.cpp
+++ ch19/a19.cpp
@@ -84,7 +84,41 @@ int main() {
     return 0;
 }
 // 실습과제4-1 ********************************************
+#include <iostream>
+using namespace std;
 
+int add(int arr[], int size) {
+    int sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+int add(int arr[], int size, int value) {
+    int sum = value;
+
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+int main() {
+    int a[] = { 1,2,3,4,5 };
+    int b[] = { 6,7,8,9,10 };
+
+    int c = add(a, 5);
+    int d = add(b, 3, c);
+
+    cout << c << endl;
+    cout << d << endl;
+
+    return 0;
+}
 // 실습과제4- *********************************************
 
 // 실습과제4- *********************************************
