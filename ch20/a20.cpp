931a3f336c4a6555a7223601db9f81a5a8abd00b
hoyoung245 <245hoyoung@gmail.com>
2026-05-28 17:44:30 +0900

    Update a20.cpp

--- ch20/a20.cpp
+++ ch20/a20.cpp
@@ -6,5 +6,43 @@
 
 // 실습과제2 **********************************************
 
+#include <iostream>
+using namespace std;
+
+class Complex {
+    int real;
+    int img;
+
+public:
+    Complex(int r = 0, int i = 0) {
+        real = r;
+        img = i;
+        cout << "복소수 " << real << "+ " << img << "j 생성" << endl;
+    }
+
+    void show() {
+        cout << real << "+" << img << "j" << endl;
+    }
+
+    friend Complex ComplexAdd(Complex x, Complex y);
+};
+
+Complex ComplexAdd(Complex x, Complex y) {
+    Complex temp;
+    temp.real = x.real + y.real;
+    temp.img = x.img + y.img;
+    return temp;
+}
+
+int main() {
+    Complex x(2, 3), y(-5, 10), sum;
+
+    sum = ComplexAdd(x, y);
+
+    cout << "두 복소수의 합은 ";
+    sum.show();
+
+    return 0;
+}
 // 실습과제3 **********************************************
 
